4caa02de58e823b9cea7917a037f700cdfb2ee0a
Diane <dchampda@student.42.fr>
2021-01-29 12:34:50 +0100

    :adhesive_bandage: Fix : New homepage and probes fixed

--- setup.sh
+++ setup.sh
@@ -32,10 +32,12 @@ kubectl apply -f srcs/metallb.yaml
 kubectl apply -f srcs/metallb-config.yaml
 
 #Deploiements et services
-kubectl apply -f srcs/nginx.yaml
-kubectl apply -f srcs/phpmyadmin.yaml
-kubectl apply -f srcs/wordpress.yaml
 kubectl apply -f srcs/mysql.yaml
+kubectl apply -f srcs/influxdb.yaml
+kubectl apply -f srcs/nginx.yaml
 kubectl apply -f srcs/ftps.yaml
 kubectl apply -f srcs/grafana.yaml
-kubectl apply -f srcs/influxdb.yaml
+kubectl apply -f srcs/phpmyadmin.yaml
+kubectl apply -f srcs/wordpress.yaml
+
+kubectl get po,svc
