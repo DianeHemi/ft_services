2fdee2a6b1763490a94642485fc121d0c45a33eb
Diane <64610122+DianeHemi@users.noreply.github.com>
2021-02-05 12:04:39 +0100

    :adhesive_bandage: Fix : sexy setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #Nettoyage, au cas ou
+printf "\033[0;32mCleaning...\033[0m\n"
 sudo minikube delete
 
 #Necessaire
@@ -9,13 +10,13 @@ sudo apt remove -y golang-docker-credential-helpers
 
 
 #Lancement de minikube
+printf "\033[0;32mStarting minikube...\033[0m\n"
 minikube start --vm-driver=none
 sleep 1
 eval $(minikube -p minikube docker-env)
 
+
 docker load -i srcs/alpine/alpine.tar
-
-
 minikube addons enable metrics-server
 minikube addons enable dashboard
 
@@ -29,6 +30,7 @@ sed -i -e "s/ipminikube/$IP/g" ./srcs/mysql/wordpress_db.sql
 rm -f srcs/ip
 
 #Build des images Docker
+printf "\033[0;32mBuilding images...\033[0m\n"
 docker build -t img-nginx srcs/nginx/
 docker build -t img-pma srcs/phpmyadmin/
 docker build -t img-wordpress srcs/wordpress/
@@ -38,6 +40,7 @@ docker build -t img-grafana srcs/grafana/
 docker build -t img-influxdb srcs/influxdb/
 
 #Lancement de metalLB
+printf "\033[0;32mLoading metallb config files...\033[0m\n"
 docker load -i srcs/alpine/controller.tar
 docker load -i srcs/alpine/speaker.tar
 kubectl apply -f srcs/metallb-namespace.yaml
@@ -49,6 +52,7 @@ kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/influxdb.yaml
 kubectl apply -f srcs/phpmyadmin.yaml
 
+printf "\033[0;32mWaiting for databases to fully initialize...\033[0m\n"
 sleep 30
 
 kubectl apply -f srcs/ftps.yaml
@@ -62,4 +66,5 @@ sed -i -e "s/$IP/ipminikube/g" ./srcs/mysql/wordpress_db.sql
 
 echo 
 echo
-echo "Useful commands : \nsudo kubectl get po,svc \nsudo minikube dashboard"
+printf "\033[0;32mUseful commands :\033[0m\n"
+printf "\033[0;33msudo kubectl get po,svc \nsudo minikube dashboard\033[0m\n"
