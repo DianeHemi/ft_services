b3230af996b2528b33a0a34bd552805abd5fe9ad
Diane <64610122+DianeHemi@users.noreply.github.com>
2021-02-05 11:51:42 +0100

    :adhesive_bandage: Fix : crash on nginx container

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Nettoyage, au cas ou
-minikube delete
+sudo minikube delete
 
 #Necessaire
 sudo apt-get install -y conntrack
@@ -26,6 +26,7 @@ IP=$(cat srcs/ip)
 sed -i -e "s/ipminikube/$IP/g" ./srcs/metallb-config.yaml
 sed -i -e "s/ipminikube/$IP/g" ./srcs/ftps/vsftpd.conf
 sed -i -e "s/ipminikube/$IP/g" ./srcs/mysql/wordpress_db.sql
+rm -f srcs/ip
 
 #Build des images Docker
 docker build -t img-nginx srcs/nginx/
@@ -37,6 +38,8 @@ docker build -t img-grafana srcs/grafana/
 docker build -t img-influxdb srcs/influxdb/
 
 #Lancement de metalLB
+docker load -i srcs/alpine/controller.tar
+docker load -i srcs/alpine/speaker.tar
 kubectl apply -f srcs/metallb-namespace.yaml
 kubectl apply -f srcs/metallb.yaml
 kubectl apply -f srcs/metallb-config.yaml
@@ -44,11 +47,14 @@ kubectl apply -f srcs/metallb-config.yaml
 #Deploiements et services
 kubectl apply -f srcs/mysql.yaml
 kubectl apply -f srcs/influxdb.yaml
-kubectl apply -f srcs/nginx.yaml
+kubectl apply -f srcs/phpmyadmin.yaml
+
+sleep 30
+
 kubectl apply -f srcs/ftps.yaml
 kubectl apply -f srcs/grafana.yaml
-kubectl apply -f srcs/phpmyadmin.yaml
 kubectl apply -f srcs/wordpress.yaml
+kubectl apply -f srcs/nginx.yaml
 
 sed -i -e "s/$IP/ipminikube/g" ./srcs/metallb-config.yaml
 sed -i -e "s/$IP/ipminikube/g" ./srcs/ftps/vsftpd.conf
